6da0156f8e6e2d61706418892c9befbfc6327137
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-29 16:13:09 +0200

    add macrostucture with comments

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,121 @@
 </script>
 
 <template>
+<!-- HEADEAR -->
+
+  <!-- UPBAR -->
+    <!-- PHONE NUMBER -->
+    <!-- ADDRESS -->
+    <!-- SOCIALS -->
+  <!-- UPBAR -->
+
+  <!-- NAVBAR -->
+    <!-- LOGO -->
+    <!-- PULSANTI DI NAVIGAZIONE -->
+    <!-- LOG IN -->
+  <!-- NAVBAR -->
+
+  <!-- JUMBO --><!-- IMG BACKBROUND -->
+    <!-- TEXT HERO -->
+  <!-- JUMBO -->
+
+  <!-- TODAYS GAME -->
+    <!-- TEXT -->
+    <!-- CARD GAME -->
+    <!-- BUTTON -->
+  <!-- TODAYS GAME -->
+
+<!-- HEADEAR -->
+
+<!-- MAIN -->
+  <!-- PRO TEAM -->
+    <!-- IMG -->
+    <!-- TEXT -->
+    <!-- DATA COMMUNITY -->
+  <!-- PRO TEAM -->
+
+  <!-- MATCHES -->
+    <!-- TEXT -->
+    <!-- TODAY MATCH -->
+    <!-- UPCOMING MATCHES -->
+      <!-- CARD SM -->
+    <!-- UPCOMING MATCHES -->
+    <!-- PREVIOUS MATCHES -->
+      <!-- CARD SM -->
+    <!-- PREVIOUS MATCHES -->
+    <!-- BUTTON -->
+  <!-- MATCHES -->
+
+  <!-- MEET SQUAD -->
+    <!-- TEXT -->
+    <!-- LIST SQUAD -->
+      <!-- CARD SQUAD -->
+    <!-- LIST SQUAD -->
+  <!-- MEET SQUAD -->
+
+  <!-- JOIN COM -->
+    <!-- TEXT -->
+    <!-- BUTTON -->
+    <!-- IMG -->
+  <!-- JOIN COM -->
+
+  <!-- LIVE STREAM -->
+    <!-- TEXT -->
+    <!-- VIDEO -->
+      <!-- CARD VIDEO -->
+    <!-- VIDEO -->
+  <!-- LIVE STREAM -->
+
+  <!-- PRODUCTS -->
+    <!-- TEXT -->
+    <!-- CARD PRODUCT -->
+  <!-- PRODUCTS -->
+
+  <!-- PARTNER -->
+    <!-- TEXT -->
+    <!-- CARD PARTNER -->
+    <!-- BUTTON -->
+  <!-- PARTNER -->
+  
+  <!-- BLOG POST -->
+    <!-- TEXT -->
+    <!-- CARD POST -->
+  <!-- BLOG POST -->
+
+  <!-- NEWS -->
+    <!-- TEXT -->
+    <!-- CARD NEWS -->
+  <!-- NEWS -->
+
+<!-- MAIN -->
+
+<!-- FOOTER -->
+  <!-- IMFORMATION -->
+  <!-- IMFORMATION -->
+
+  <!-- FOOTER BODY -->
+    <!-- SPECIFICS BRANDS -->
+      <!-- IMG -->
+      <!-- TEXT -->
+      <!-- SOCIALS -->
+    <!-- SPECIFICS BRANDS -->
+
+    <!-- TOP GAME -->
+      <!-- TEXT -->
+      <!-- CARD GAME -->
+    <!-- TOP GAME -->
+
+    <!-- NEWSLETTER -->
+      <!-- TEXT -->
+      <!-- FORM -->
+    <!-- NEWSLETTER -->
+
+
+  <!-- FOOTER BODY -->
+
+  <!-- COPYRIGHT -->
+<!-- FOOTER -->
+
 </template>
 
 <style scoped>
