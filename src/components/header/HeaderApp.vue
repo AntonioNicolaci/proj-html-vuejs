e2ebb71215ece0874388d28aeb9f594202787ad6
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-30 11:48:49 +0200

    add structure file

---
+++ src/components/header/HeaderApp.vue
@@ -0,0 +1,23 @@
+<script>
+import NavbarHeaderApp from './NavbarHeaderApp.vue';
+
+export default {
+    components: {
+        NavbarHeaderApp,
+    }
+}
+</script>
+
+<template>
+    <header>
+        <NavbarHeaderApp />
+        <!-- jumbo -->   
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    header {
+        background: url('../../../public/img/bg.jpg') no-repeat;
+        height: 50vw;
+    }
+</style>
