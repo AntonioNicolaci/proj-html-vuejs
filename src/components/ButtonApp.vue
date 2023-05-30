96a2881e0b6b3f54bbe950c00172a74b158bf1fc
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-30 11:47:59 +0200

    add file and structure for dinamic button

---
+++ src/components/ButtonApp.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: {
+        text: String,
+        link: String,
+        classBTN: Array,
+    }
+}
+</script>
+
+<template>
+    <a :href="link"
+    :class="classBTN"
+    >{{ text }}</a>
+</template>
+
+<style lang="scss" scoped>
+a{
+    text-decoration: none;
+}
+.whiteBTN{
+    color: #fff;
+    font-weight: bold;
+}
+</style>
