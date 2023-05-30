72c03f3896fe6454215c70e803dfe5e9c49027e4
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-30 22:26:25 +0200

    add file

---
+++ src/components/BTNSocialVideo.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        Vimeo: String,
+        YT: String,
+        Twitch: String,
+    }
+}
+</script>
+<template>
+    <div>
+        <a :href="Vimeo">
+            <img src="../../public/img/social-1.png" alt="vimeo">
+        </a>
+        <a :href="YT">
+            <img src="../../public/img/social-2.png" alt="yt">
+        </a>
+        <a :href="Twitch">
+            <img src="../../public/img/social-3.png" alt="twitch">
+        </a>
+    </div>
+</template>
+<style lang="scss" scoped>
+    div{
+        display: flex;
+        gap: 1em;
+    }
+</style>
