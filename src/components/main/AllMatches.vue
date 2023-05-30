e5543fa35fc54bb5c3f6979bb8d4ebe594c21d4c
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-30 22:28:07 +0200

    add arry object/ add components for matches

--- src/components/main/AllMatches.vue
+++ src/components/main/AllMatches.vue
@@ -1,8 +1,128 @@
 <script>
 import ButtonApp from '../ButtonApp.vue';
+import FirstMatch from './FirstMatch.vue';
+import AnMatch from './AnMatch.vue';
 
 export default {
-    components: { ButtonApp }
+    data() {
+        return {
+            allMatch: [
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-6.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-9.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-1.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-2.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-3.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-4.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-5.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-6.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-7.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-8.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-9.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-10.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+                {
+                    data: '30 April 2022',
+                    time: '08:30PM',
+                    game: 'BATTLEFIELD-4 TOURNAMENT',
+                    team1: '',
+                    logoTeam1: 'teamsm-11.png',
+                    team2: '',
+                    logoTeam2: 'teamsm-12.png',
+                    group: '2',
+                    player: '32',
+                    linkV: '#',
+                    linkYT: '#',
+                    linkT: '#',
+                    linkNow: '#',
+                    pricePool: 3200,
+                },
+            ]
+        }
+    },
+    components: { ButtonApp, FirstMatch, AnMatch }
 }
 </script>
 <template>
@@ -10,24 +130,30 @@ export default {
         <p>
             ANYWHERE, ANYTINE
         </p>
-        <h2></h2>
-        <FirstMatch />
+        <h2>
+            ALL MATCHES SCHEDULE
+        </h2>
+        <FirstMatch :data="allMatch[0].data" :time="allMatch[0].time" :game="allMatch[0].game"
+            :logoTeam1="allMatch[0].logoTeam1" :logoTeam2="allMatch[0].logoTeam2" :group="allMatch[0].group"
+            :player="allMatch[0].player" :linkV="allMatch[0].linkV" :linkYT="allMatch[0].linkYT" :linkT="allMatch[0].linkT"
+            :linkNow="allMatch[0].linkNow" :pricePools="allMatch[0].pricePool" />
         <div>
             <div>
-                <h3></h3>
-                <AnMatch />
+                <h3>UPCOMIC MATCHES</h3>
+                <AnMatch v-for="n in 2" :data="allMatch[n + 1].data" :time="allMatch[n + 1].time" :game="allMatch[n + 1].game"
+                    :logoTeam1="allMatch[n + 1].logoTeam1" :logoTeam2="allMatch[n + 1].logoTeam2" :group="allMatch[n + 1].group"
+                    :player="allMatch[n + 1].player" :linkV="allMatch[n + 1].linkV" :linkYT="allMatch[n + 1].linkYT"
+                    :linkT="allMatch[n + 1].linkT" :linkNow="allMatch[n + 1].linkNow" :pricePools="allMatch[n + 1].pricePool" />
             </div>
             <div>
-                <h3></h3>
-                <AnMatch />
+                <h3>PREVIOUS MATCHES</h3>
+                <AnMatch v-for="n in 2" :data="allMatch[n + 4].data" :time="allMatch[n + 4].time" :game="allMatch[n + 4].game"
+                    :logoTeam1="allMatch[n + 4].logoTeam1" :logoTeam2="allMatch[n + 4].logoTeam2" :group="allMatch[n + 4].group"
+                    :player="allMatch[n + 4].player" :linkV="allMatch[n + 4].linkV" :linkYT="allMatch[n + 4].linkYT"
+                    :linkT="allMatch[n + 4].linkT" :linkNow="allMatch[n + 4].linkNow" :pricePools="allMatch[n + 4].pricePool" />
             </div>
         </div>
-        <ButtonApp
-        :text="'BROWSER ALL MATCHES'"
-        :link="'#'"
-        :classCSS="[]" />
+        <ButtonApp :text="'BROWSER ALL MATCHES'" :link="'#'" :classCSS="[]" />
     </div>
 </template>
-<style lang="scss" scoped>
-    
-</style>
+<style lang="scss" scoped></style>
