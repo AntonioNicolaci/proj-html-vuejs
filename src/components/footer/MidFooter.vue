083ef3ec2af138779d408821634d2fc49806c79a
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:30:56 +0200

    add arr OBJ/ add style

--- src/components/footer/MidFooter.vue
+++ src/components/footer/MidFooter.vue
@@ -7,22 +7,22 @@ export default {
         return {
             JPCard: [
                 {
-                    logo:'01 (4).jpg',
-                    nameG:'FREE POKER GAME',
-                    poker:230,
-                    rating:5,
+                    logo: '01 (4).jpg',
+                    nameG: 'FREE POKER GAME',
+                    poker: 230,
+                    rating: 5,
                 },
                 {
-                    logo:'01 (4).jpg',
-                    nameG:'CLUB POKER GAME',
-                    poker:290,
-                    rating:5,
+                    logo: '01 (4).jpg',
+                    nameG: 'CLUB POKER GAME',
+                    poker: 290,
+                    rating: 5,
                 },
                 {
-                    logo:'01 (4).jpg',
-                    nameG:'ROYAL POKER GAME',
-                    poker:330,
-                    rating:5,
+                    logo: '01 (4).jpg',
+                    nameG: 'ROYAL POKER GAME',
+                    poker: 330,
+                    rating: 5,
                 },
             ]
         }
@@ -35,25 +35,52 @@ export default {
 }
 </script>
 <template>
-    <div>
-        <img src="../../../public/img/logo.png" alt="logo">
-        <p>
-            Upropriate brand economca sound technolog after covalent technology enable prospective wastng markets whereas propriate and brand economca sound technolog
-        </p>
-        <BTNSocialVideo :Vimeo="'#'" :YT="'#'" :Twitch="'#'" />
-    </div>
-    <div>
-        <h2>TOP JACKPOT GAMES</h2>
-        <JPCard v-for="JPC in JPCard" :logo="JPC.logo" :nameG="JPC.nameG" :poker="JPC.poker" :rating="JPC.rating"/>
-    </div>
-    <div>
-        <h2>OUR NEWSLETTER</h2>
-        <p>Gamer esports organization supported by community leaders</p>
-        <input type="text">
-        <input type="text">
-        <ButtonApp :text="'SEND MASSAGE'" :link="'#'" :classCSS=[] />
+    <div id="MFDiv">
+        <div id="logoDiv">
+            <img src="../../../public/img/logo.png" alt="logo">
+            <p>
+                Upropriate brand economca sound technolog after covalent technology enable prospective wastng markets
+                whereas propriate and brand economca sound technolog
+            </p>
+            <BTNSocialVideo :Vimeo="'#'" :YT="'#'" :Twitch="'#'" />
+        </div>
+        <div id="JPDiv">
+            <h2>TOP JACKPOT GAMES</h2>
+            <JPCard v-for="JPC in JPCard" :logo="JPC.logo" :nameG="JPC.nameG" :poker="JPC.poker" :rating="JPC.rating" />
+        </div>
+        <div id="formDiv">
+            <h2>OUR NEWSLETTER</h2>
+            <p>Gamer esports organization supported by community leaders</p>
+            <input type="text">
+            <input type="text">
+            <ButtonApp :text="'SEND MASSAGE'" :link="'#'" :classCSS=[] />
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+#MFDiv {
+    display: flex;
+    padding-block: 6em;
+}
+
+#logoDiv, #JPDiv, #formDiv {
+    display: flex;
+    flex-direction: column;
+    width: calc(100% / 3 - 1em);
+}
+
+#logoDiv{
+    & img {
+        width: 180px;
+    }
+    & p {
+        max-width: 200px;
+    }
+}
+
+#JPDiv {
+
+}
+
+#formDiv {}
 </style>
