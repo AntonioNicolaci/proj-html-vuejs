9bf3626526f0d4b16542f65e6b32a6046a75c402
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:09:14 +0200

    add style/ add decoration

--- src/components/main/TodayCard.vue
+++ src/components/main/TodayCard.vue
@@ -8,16 +8,27 @@ export default {
 }
 </script>
 <template>
-    <div class="card">
-        <img :src="`../../../public/img/${team1}`" alt="team1">
-        <img src="../../../public/img/vs.png" alt="vs">
-        <img :src="`../../../public/img/${team2}`" alt="team2">
+    <div>
+        <div class="card">
+            <img :src="`../../../public/img/${team1}`" alt="team1">
+            <img src="../../../public/img/vs.png" alt="vs" class="vs">
+            <img :src="`../../../public/img/${team2}`" alt="team2">
+        </div>
+        <div class="divLL">
+            <div class="lungo">
+                <div class="largo"></div>
+            </div>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    .card {
-        background-color: #232a5c;
-        display: flex;
-        flex-direction: row;
-    }
+.card {
+    background-color: #232a5c;
+    display: flex;
+    align-items: center;
+    padding: 3em 2em;
+    border-radius: 8px;
+    gap: 1em;
+    max-height: 12em;
+}
 </style>
