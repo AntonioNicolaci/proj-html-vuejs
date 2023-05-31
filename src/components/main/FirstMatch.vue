18beecd93b85da585ef5d17569b9027110903635
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:08:24 +0200

    add style

--- src/components/main/FirstMatch.vue
+++ src/components/main/FirstMatch.vue
@@ -23,14 +23,14 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div id="FSDiv">
         <div class="FMCHeader">
             <div>
                 <span>{{ group }} GROUP</span>
                 <span>{{ player }} PLAYERS</span>
             </div>
             <div>
-                <span>PRICE POOl ${{ pricePool }}</span>
+                <span>PRICE POOL ${{ pricePool }}</span>
             </div>
         </div>
         <div class="FMCBody">
@@ -40,18 +40,65 @@ export default {
                     <span class="FMCBData">{{ data }}</span> <span class="FMCBTime">Time: {{ time }}</span>
                 </div>
             </div>
-            <div>
+            <div class="FMImg">
                 <img :src="`../../../public/img/${logoTeam1}`" alt="team-1">
                 <img src="../../../public/img/vs.png" alt="vs">
                 <img :src="`../../../public/img/${logoTeam2}`" alt="team-2">
             </div>
-            <div>
+            <div class="FMBTN">
                 <BTNSocialVideo :Vimeo="linkV" :TY="linkYT" :Twitch="linkT" />
+                <ButtonApp :text="'WATCH NOW'" :link="'#'" :ClassCSS=[] />
             </div>
-            <ButtonApp :text="'WATCH NOW'" :link="'#'" :ClassCSS=[] />
+        </div>
+    </div>
+    <div class="divLL">
+        <div class="lungo">
+            <div class="largo"></div>
         </div>
     </div>
 </template>
 <style lang="scss" scoped>
-.FMCHeader {}
-</style>
+#FSDiv {
+    width: 100%;
+}
+
+.FMCHeader {
+    display: flex;
+    justify-content: space-between;
+    background-color: #ff0052;
+    padding: 1em 1.5em;
+}
+
+.FMCBody {
+    display: flex;
+    justify-content: space-between;
+    background-color: #fff;
+    color: #000;
+    align-items: center;
+    padding: 2em;
+}
+
+.FMCBGame {
+    font-weight: bold;
+}
+
+.FMCBData {
+    padding-right: 1em;
+    border-right: 2px solid #999;
+}
+
+.FMCBTime {
+    padding-left: 1em;
+}
+
+.FMImg {
+    display: flex;
+    align-items: center;
+    gap: 3em;
+}
+
+.FMBTN {
+    display: flex;
+    align-items: center;
+    gap: 1em
+}</style>
