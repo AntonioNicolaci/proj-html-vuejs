79b9fc6d2b76ce81b9d6a31dd5ccbfcdb1b04d52
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:05:49 +0200

    add footer/ remove comments

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import HeaderApp from './components/header/HeaderApp.vue';
 import MainApp from './components/main/MainApp.vue';
+import FooterApp from './components/footer/FooterApp.vue';
 
 export default {
   components: {
     HeaderApp,
     MainApp,
+    FooterApp,
   }
 }
 
@@ -15,117 +17,7 @@ export default {
 
   <headerApp />
   <MainApp />
-<!-- HEADEAR -->
-
-  <!-- UPBAR -->
-    <!-- PHONE NUMBER -->
-    <!-- ADDRESS -->
-    <!-- SOCIALS -->
-  <!-- UPBAR -->
-
-  <!-- NAVBAR -->
-    <!-- LOGO -->
-    <!-- PULSANTI DI NAVIGAZIONE -->
-    <!-- LOG IN -->
-  <!-- NAVBAR -->
-
-  <!-- JUMBO --><!-- IMG BACKBROUND -->
-    <!-- TEXT HERO -->
-  <!-- JUMBO -->
-<!-- HEADEAR -->
-
-<!-- MAIN -->
-  <!-- TODAYS GAME -->
-    <!-- TEXT -->
-    <!-- CARD GAME -->
-    <!-- BUTTON -->
-  <!-- TODAYS GAME -->
-  
-  <!-- PRO TEAM -->
-    <!-- IMG -->
-    <!-- TEXT -->
-    <!-- DATA COMMUNITY -->
-  <!-- PRO TEAM -->
-
-  <!-- MATCHES -->
-    <!-- TEXT -->
-    <!-- TODAY MATCH -->
-    <!-- UPCOMING MATCHES -->
-      <!-- CARD SM -->
-    <!-- UPCOMING MATCHES -->
-    <!-- PREVIOUS MATCHES -->
-      <!-- CARD SM -->
-    <!-- PREVIOUS MATCHES -->
-    <!-- BUTTON -->
-  <!-- MATCHES -->
-
-  <!-- MEET SQUAD -->
-    <!-- TEXT -->
-    <!-- LIST SQUAD -->
-      <!-- CARD SQUAD -->
-    <!-- LIST SQUAD -->
-  <!-- MEET SQUAD -->
-
-  <!-- JOIN COM -->
-    <!-- TEXT -->
-    <!-- BUTTON -->
-    <!-- IMG -->
-  <!-- JOIN COM -->
-
-  <!-- LIVE STREAM -->
-    <!-- TEXT -->
-    <!-- VIDEO -->
-      <!-- CARD VIDEO -->
-    <!-- VIDEO -->
-  <!-- LIVE STREAM -->
-
-  <!-- PRODUCTS -->
-    <!-- TEXT -->
-    <!-- CARD PRODUCT -->
-  <!-- PRODUCTS -->
-
-  <!-- PARTNER -->
-    <!-- TEXT -->
-    <!-- CARD PARTNER -->
-    <!-- BUTTON -->
-  <!-- PARTNER -->
-  
-  <!-- BLOG POST -->
-    <!-- TEXT -->
-    <!-- CARD POST -->
-  <!-- BLOG POST -->
-
-  <!-- NEWS -->
-    <!-- TEXT -->
-    <!-- CARD NEWS -->
-  <!-- NEWS -->
-
-<!-- MAIN -->
-
-<!-- FOOTER -->
-  <!-- IMFORMATION -->
-  <!-- IMFORMATION -->
-
-  <!-- FOOTER BODY -->
-    <!-- SPECIFICS BRANDS -->
-      <!-- IMG -->
-      <!-- TEXT -->
-      <!-- SOCIALS -->
-    <!-- SPECIFICS BRANDS -->
-
-    <!-- TOP GAME -->
-      <!-- TEXT -->
-      <!-- CARD GAME -->
-    <!-- TOP GAME -->
-
-    <!-- NEWSLETTER -->
-      <!-- TEXT -->
-      <!-- FORM -->
-    <!-- NEWSLETTER -->
-  <!-- FOOTER BODY -->
-
-  <!-- COPYRIGHT -->
-<!-- FOOTER -->
+  <FooterApp />
 
 </template>
 
