ea203ace761c2819cbb1364e306542a6d39533a0
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:07:35 +0200

    fix another  matches rendering

--- src/components/main/AllMatches.vue
+++ src/components/main/AllMatches.vue
@@ -126,7 +126,7 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div id="AllMDiv" class="align">
         <p>
             ANYWHERE, ANYTINE
         </p>
@@ -137,23 +137,42 @@ export default {
             :logoTeam1="allMatch[0].logoTeam1" :logoTeam2="allMatch[0].logoTeam2" :group="allMatch[0].group"
             :player="allMatch[0].player" :linkV="allMatch[0].linkV" :linkYT="allMatch[0].linkYT" :linkT="allMatch[0].linkT"
             :linkNow="allMatch[0].linkNow" :pricePools="allMatch[0].pricePool" />
-        <div>
+        <div id="ANMatchDiv">
             <div>
                 <h3>UPCOMIC MATCHES</h3>
-                <AnMatch v-for="n in 2" :data="allMatch[n + 1].data" :time="allMatch[n + 1].time" :game="allMatch[n + 1].game"
-                    :logoTeam1="allMatch[n + 1].logoTeam1" :logoTeam2="allMatch[n + 1].logoTeam2" :group="allMatch[n + 1].group"
-                    :player="allMatch[n + 1].player" :linkV="allMatch[n + 1].linkV" :linkYT="allMatch[n + 1].linkYT"
-                    :linkT="allMatch[n + 1].linkT" :linkNow="allMatch[n + 1].linkNow" :pricePools="allMatch[n + 1].pricePool" />
+                <AnMatch v-for="n in 3" :data="allMatch[n].data" :time="allMatch[n].time" :game="allMatch[n].game"
+                    :logoTeam1="allMatch[n].logoTeam1" :logoTeam2="allMatch[n].logoTeam2" :group="allMatch[n].group"
+                    :player="allMatch[n].player" :linkV="allMatch[n].linkV" :linkYT="allMatch[n].linkYT"
+                    :linkT="allMatch[n].linkT" :linkNow="allMatch[n].linkNow" :pricePools="allMatch[n].pricePool" />
             </div>
             <div>
                 <h3>PREVIOUS MATCHES</h3>
-                <AnMatch v-for="n in 2" :data="allMatch[n + 4].data" :time="allMatch[n + 4].time" :game="allMatch[n + 4].game"
-                    :logoTeam1="allMatch[n + 4].logoTeam1" :logoTeam2="allMatch[n + 4].logoTeam2" :group="allMatch[n + 4].group"
-                    :player="allMatch[n + 4].player" :linkV="allMatch[n + 4].linkV" :linkYT="allMatch[n + 4].linkYT"
-                    :linkT="allMatch[n + 4].linkT" :linkNow="allMatch[n + 4].linkNow" :pricePools="allMatch[n + 4].pricePool" />
+                <AnMatch v-for="n in 3" :data="allMatch[n + 3].data" :time="allMatch[n + 3].time" :game="allMatch[n + 3].game"
+                    :logoTeam1="allMatch[n + 3].logoTeam1" :logoTeam2="allMatch[n + 3].logoTeam2" :group="allMatch[n + 3].group"
+                    :player="allMatch[n + 3].player" :linkV="allMatch[n + 3].linkV" :linkYT="allMatch[n + 3].linkYT"
+                    :linkT="allMatch[n + 3].linkT" :linkNow="allMatch[n + 3].linkNow" :pricePools="allMatch[n + 3].pricePool" />
             </div>
         </div>
         <ButtonApp :text="'BROWSER ALL MATCHES'" :link="'#'" :classCSS="[]" />
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#AllMDiv {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    & h3{
+        margin-block: 6em 2em;
+    }
+}
+#ANMatchDiv {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    gap: 2em;
+    & div{
+        text-align: center;
+    }
+}
+</style>
