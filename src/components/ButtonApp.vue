eb6d2bae880eae6fcc312af7a89d3c2d9a3363a9
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:50:55 +0200

    add style

--- src/components/ButtonApp.vue
+++ src/components/ButtonApp.vue
@@ -9,16 +9,37 @@ export default {
 </script>
 
 <template>
-    <a :href="link"
-    :class="classBTN"
-    >{{ text }}</a>
+    <a :href="link" :class="classBTN">{{ text }}</a>
 </template>
 
 <style lang="scss" scoped>
-a{
+a {
     text-decoration: none;
+    padding: 1em;
 }
-.whiteBTN{
+
+.whiteBTN {
     font-weight: bold;
+    color: #fff;
+}
+.whiteBG {
+    background-color: #fff;
+}
+.redBG{
+    background-color: #ff0052;
+}
+.whiteColor{
+    color: #fff;
+}
+.redColor {
+    color: #ff0052;
+}
+.redBorder{
+    border-bottom: 2px solid #ff0052;
+    border-right: 2px solid #ff0052;
+}
+.whiteBorder{
+    border-bottom: 2px solid #fff;
+    border-right: 2px solid #fff;
 }
 </style>
