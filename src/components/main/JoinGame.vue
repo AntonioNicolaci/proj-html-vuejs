18e861c81e95a7b87122ae741802eb70ba291c01
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-31 17:08:38 +0200

    add style

--- src/components/main/JoinGame.vue
+++ src/components/main/JoinGame.vue
@@ -6,20 +6,41 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div id="JGDiv" class="align">
         <div>
             <p>
-
+                ,ET'S PLAY TOGETHER
             </p>
             <h2>
-
+                JOIN <span>GAMER</span> ESPORTS TO BECOME NEXT PRO GAMER TODAY!
             </h2>
-            <p></p>
-            <ButtonApp />
+            <p>Distinctively provide acces mutfunct users whereas transparent proceses somes ncentivize eficient functionalities rather than an extensible archtectur services and cross</p>
+            <ButtonApp :text="'JOIN COMMUNITY'" :link="'#'" :classCSS=[] />
         </div>
-        <img src="" alt="">
+        <img src="../../../public/img/01 (1).png" alt="img">
+    </div>
+    <div class="divLL">
+        <div class="lungo">
+            <div class="largo"></div>
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    #JGDiv {
+        display: flex;
+        align-items: center;
+        background: url('../../../public/img/bg (4).jpg') no-repeat 50%;
+
+        & div {
+            display: flex;
+            flex-direction: column;
+            row-gap: 1em;
+        }
+    }
+    h2 span {
+        color: #ff0052;
+    }
+    p{
+        color: #fff;
+    }
 </style>
